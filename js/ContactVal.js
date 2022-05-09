04f270a02f82dcc7513625f7f89c6c758463b0ad
EdinGrech <edin.grech@gmail.com>
2022-05-09 16:55:07 +0200

    Fixed moble end .... again (still need to add a burger menu ) added contact page with validation and stuff should be moble friendly and nothing breaks but not yet tested on devices

---
+++ js/ContactVal.js
@@ -0,0 +1,47 @@
+//contact for validation
+$("form").submit(function(){
+    let name = $("#name").val();
+    let email = $("#mail").val();
+    let phone = $("#phone").val();
+    let message = $("#message").val();
+    let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if(name == ""){
+        $("#name").focus();
+        $("#name").css("border","1px solid red");
+        $("#name").css("box-shadow","0 0 10px red");
+        $("#name").attr("placeholder","Please enter your name");
+        return false;
+    }
+    else if(email == ""){
+        $("#mail").focus();
+        $("#mail").css("border","1px solid red");
+        $("#mail").css("box-shadow","0 0 10px red");
+        $("#mail").attr("placeholder","Please enter your email");
+        return false;
+    }
+    else if(!filter.test(email)){
+        $("#mail").focus();
+        $("#mail").css("border","1px solid red");
+        $("#mail").css("box-shadow","0 0 10px red");
+        $("#mail").attr("placeholder","Please enter a valid email");
+        return false;
+    }
+    else if(isNaN(phone) && phone.lenght() >= 8){ 
+        $("#phone").focus();
+        $("#phone").css("border","1px solid red");
+        $("#phone").css("box-shadow","0 0 10px red");
+        $("#phone").attr("placeholder","Please enter your phone number");
+        return false;
+    }
+    else if(message == ""){
+        $("#message").focus();
+        $("#message").css("border","1px solid red");
+        $("#message").css("box-shadow","0 0 10px red");
+        $("#message").attr("placeholder","Please enter your message");
+        return false;
+    }
+    else{
+        return true;
+    }
+})
--- js/Prod.js
+++ js/Prod.js
@@ -1,23 +1,37 @@
 let flip = false;
+var direction = $(".prodCard").css("flex-direction");
 
 $(".info").click(function(){
     var otherInput = $(this).closest('.prodCard').find('.infoPopOut');
+    console.log(direction)
     if (flip == false) {
         otherInput.css("opacity", "1").css("padding-left","60px").css("visability","visible");
-    flip = true;
+        otherInput.height("100%");
+        flip = true;
     }
     else {
         otherInput.css("opacity", "0").css("padding-left","0px").css("visability","hidden");
+        otherInput.height(0);
         flip = false;
     }
 });
 
 $().ready(function(){
+    $('.infoPopOut').height(0)
     $('.container').height($('.container').width());
     $('.overlay p').height($('.container').width());
-});
+    if (direction == "column") {
+        $(".bottemSpacer").height($("#LastP").height());
+    }});
 
 window.addEventListener('resize', function(event) {
-    $('.container').height($('.container').width());
-    $('.overlay p').height($('.container').width());
+    var direction = $(".prodCard").css("flex-direction");
+    console.log(direction)
+    if (direction != "coloum"){
+        $('.container').height($('.container').width());
+        $('.overlay p').height($('.container').width());    }
+    else{
+        $('.container').height("auto");
+        $('.overlay p').height("auto");
+    }
 }, true);
