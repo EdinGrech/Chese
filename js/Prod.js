bf00b0bd37823434b678ab87bfc5d6936e254f23
EdinGrech <edin.grech@gmail.com>
2022-05-10 16:35:22 +0200

    minor changes and tweeks to layout

--- js/Prod.js
+++ js/Prod.js
@@ -19,7 +19,7 @@ $(".info").click(function(){
 $("#navCheeseSvg").click(function(){
     if (flip == false) {
         $(".menu__list").css("display", "block").css("position", "fixed").css("top", "0").css("left", "0").css("width", "30%").css("height", "100%").css("z-index", "100").css("background-color", "rgba(240, 240, 240,0.95)");
-        $("#navCheeseSvg").css("width", "40%").css("padding-left","22%").css("padding-top","18%");
+        $("#navCheeseSvg").css("width", "40%").css("padding-top","18%");
         $(".menu__group").css("display","flex");
         flip = true;
     }
