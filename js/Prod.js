90215bfc32c6858a8a061c639989be95afa20f25
EdinGrech <edin.grech@gmail.com>
2022-05-10 16:06:29 +0200

    added amburger menu, js imports are hard so I just put the amburger js into the hovcss I hate it but it will have to do, 99% sure teacher is not eve ngona realize there is a spageti monster cooking in the kitchen

--- js/Prod.js
+++ js/Prod.js
@@ -16,8 +16,23 @@ $(".info").click(function(){
     }
 });
 
+$("#navCheeseSvg").click(function(){
+    if (flip == false) {
+        $(".menu__list").css("display", "block").css("position", "fixed").css("top", "0").css("left", "0").css("width", "30%").css("height", "100%").css("z-index", "100").css("background-color", "rgba(240, 240, 240,0.95)");
+        $("#navCheeseSvg").css("width", "40%").css("padding-left","22%").css("padding-top","18%");
+        $(".menu__group").css("display","flex");
+        flip = true;
+    }
+    else {
+        $(".menu__list").removeAttr("style");
+        $("#navCheeseSvg").removeAttr("style");
+        $(".menu__group").removeAttr("style");
+        flip = false;
+    }
+});
+
 $().ready(function(){
-    $('.infoPopOut').height(0)
+    $('.infoPopOut').height(0);
     $('.container').height($('.container').width());
     $('.overlay p').height($('.container').width());
     if (direction == "column") {
---
+++ js/amBurgerMenu.js
@@ -0,0 +1,16 @@
+let flip = false;
+
+$("#navCheeseSvg").click(function(){
+    if (flip == false) {
+        $(".menu__list").css("display", "block").css("position", "fixed").css("top", "0").css("left", "0").css("width", "30%").css("height", "100%").css("z-index", "100").css("background-color", "rgba(240, 240, 240,0.95)");
+        $("#navCheeseSvg").css("width", "40%").css("padding-left","22%").css("padding-top","18%");
+        $(".menu__group").css("display","flex");
+        flip = true;
+    }
+    else {
+        $(".menu__list").removeAttr("style");
+        $("#navCheeseSvg").removeAttr("style");
+        $(".menu__group").removeAttr("style");
+        flip = false;
+    }
+});
